01fe28d2582d1387d631b1b1b8816858476522e7
smcc93 <smccalle@iu.edu>
2021-04-15 13:08:26 -0400

    Update shop.component.jsx

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -6,19 +6,15 @@ import CollectionPageContainer from "../collection/collection.container";
 import { connect } from "react-redux";
 import { fetchCollectionsStartAsync } from "../../redux/shop/shop.actions";
 
-
 class ShopPage extends React.Component {
-
-
   componentDidMount() {
-    const { fetchCollectionsStartAsync} = this.props;
+    const { fetchCollectionsStartAsync } = this.props;
     fetchCollectionsStartAsync();
-    
   }
 
   render() {
     const { match } = this.props;
-    
+
     return (
       <div className="shop-page">
         <Route
@@ -35,10 +31,8 @@ class ShopPage extends React.Component {
   }
 }
 
-
-
 const mapDispatchToProps = (dispatch) => ({
-  fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
+  fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync()),
 });
 
 export default connect(null, mapDispatchToProps)(ShopPage);
